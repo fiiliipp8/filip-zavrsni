77a7c5eca136f02b633e6e0dd3c59f15fe2557b0
fiiliipp8 <134615154+fiiliipp8@users.noreply.github.com>
2023-05-25 17:12:36 +0200

    Create funkcije.c

---
+++ funkcije.c
@@ -0,0 +1,41 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "header.h"
+
+
+void ispis_izbornika() {
+
+	printf("ZAPNISNIK NOGOMETNE UTAKMICE\n");
+	printf("1. Unesi igrace domace momcadi\n");
+	printf("2. Unesi igrace gostujuce momcadi\n");
+	printf("3. Unesi mjesto igranja utakmice\n");
+	printf("4. Unesi vrijeme pocetka utakmice\n");
+	printf("5. Unesi strijelce\n");
+	printf("6. Unesi konacni rezultat\n");
+}
+
+
+void igraci_prve() {
+	FILE* pokazivacDatoteke = fopen("igraci.txt", "a");
+	if (pokazivacDatoteke == NULL) {
+		printf("Ne mogu otvoriti dadoteku\n");
+		return;
+	}
+	IGRAC c;
+	
+	printf("Unesite ime: ");
+	scanf("%s", c.ime);
+	printf("Unesite prezime: ");
+	scanf("%s", c.prezime);
+	printf("Unesite broj: ");
+	scanf("%s", c.broj);
+	
+	
+	fwrite(&c, sizeof(c), 1, pokazivacDatoteke);
+
+	fclose(pokazivacDatoteke);
+	printf("Igrac dodan.\n");
+}
+
