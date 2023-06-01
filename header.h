bf100488caa700b15072ef38ec4d1ca0fae4f957
fiiliipp8 <134615154+fiiliipp8@users.noreply.github.com>
2023-06-01 17:16:30 +0200

    Update header.h

--- header.h
+++ header.h
@@ -3,19 +3,18 @@
 #define HEADER_H
 
 typedef struct igrac {
-	
+
 	char ime[20];
 	char prezime[20];
 	int broj;
-	
+
 }IGRAC;
 
 void ispis_izbornika();
 void igraci_domace();
 void igraci_gostujuce();
-void mjesto_igranja();
-void vrijeme_pocetka();
-void strijelci();
-void konacni_rezultat();
+void ispisi_igrace();
+void izbrisi_igraca();
+
 
 #endif
