56e56ec98d6720894db394cb1e7a74a4dd3dabfd
fiiliipp8 <134615154+fiiliipp8@users.noreply.github.com>
2023-07-05 21:00:04 +0200

    Update header.h

--- header.h
+++ header.h
@@ -1,7 +1,7 @@
 #ifndef MYHEADER
 #define MYHEADER
 
-typedef struct IGRAC { //2 //3
+typedef struct igrac { 
 	unsigned int brojac;
 	char Ime[51];
 	char Prezime[51];
@@ -11,5 +11,12 @@ typedef struct IGRAC { //2 //3
 
 void kreiranje(char* datoteka, unsigned int* pBrojIgraca);
 void glavniIzbornik(char* datoteka, unsigned int* BrojIgraca);
+void pocetniIzbornik();
+void dodajIgraca(char* datoteka, unsigned int* brojIgraca);
+void ispisiIgrace(char* datoteka, unsigned int* brojIgraca);
+void pronalazenjeIgraca(char* datoteka, unsigned int* brojIgraca);
+void uredivanjeIgraca(char* datoteka, unsigned int* brojIgraca);
+void sortiranjeImena(char* datoteka, unsigned int* brojIgraca);
+void izlazFunkcija(void);
 
-#endif 
+#endif
